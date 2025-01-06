7744261de13da7aad5a6d3b0f94407b9c6118b57
Nicolas Raillard <nicolas.raillard@ifremer.fr>
2025-01-06 16:32:56 +0100

    first commit

---
+++ create_webR_repo.R
@@ -0,0 +1,34 @@
+# Docker startup:
+# docker run -it --rm -v ${PWD}/output:/output -w /output ghcr.io/r-wasm/webr:main R
+
+# https://docs.github.com/en/actions/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
+# > When a container action runs, it will automatically map the default working directory (GITHUB_WORKSPACE) on the runner with the /github/workspace directory on the container. Any files added to this directory on the container will be available to any subsequent steps in the same job.
+# local_rwasm_dir <- "_rwasm"
+# gha_dir <- file.path("/github/workspace", local_rwasm_dir)
+
+gha_dir <- file.path("/github/workspace")
+
+withr::local_dir(gha_dir)
+
+install.packages("pak")
+pak::pak("r-wasm/rwasm")
+
+library(rwasm)
+
+oldopt = options(
+  repos = c(
+         CRAN = "https://cran.rstudio.com",
+         packman = "https://packagemanager.posit.co/cran/__linux__/noble/latest",
+         runiverse = "https://resourcecode-project.r-universe.dev",
+         cloud = "https://cloud.r-project.org",
+         wasm = 'https://repo.r-wasm.org'
+    )
+)
+
+pak::meta_update()
+
+rwasm::add_pkg("resourcecode",dependencies = TRUE)
+rwasm::add_pkg("hexbin")
+rwasm::add_pkg("openair")
+
+rwasm::make_vfs_library(compress = TRUE)
